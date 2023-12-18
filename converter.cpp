d70b04a34a72492e6d9d2aa2845562520142dbdd
Michael Jacobs <M.S.Jacobs2727@gmail.com>
2023-12-18 14:08:39 -0500

    make CMakeLists and converter app for testing

---
+++ converter.cpp
@@ -0,0 +1,30 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+#include "binaryFile.h"
+#include "game.h"
+#include "pgnFile.h"
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+        return 1;
+
+    std::string input_filename{argv[1]};
+    std::string output_filename{argv[2]};
+
+    std::ifstream input{input_filename};
+    auto game = ChessGame::readPGN(input);
+    input.close();
+
+    std::ofstream output{output_filename};
+    ChessGame::writeBinary(output, game);
+    output.close();
+
+    input.open(output_filename);
+    auto game2 = ChessGame::readBinary(input);
+    input.close();
+
+    return 0;
+}
