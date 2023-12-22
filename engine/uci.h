0512e8c001b4adc8c26d6e5a436ae44d6f01f20e
Michael Jacobs <M.S.Jacobs2727@gmail.com>
2023-12-22 12:15:50 -0500

    create engine interface

---
+++ engine/uci.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <boost/process.hpp>
+
+#include "chessgame/evaluation.h"
+#include "chessgame/game.h"
+#include "chessgame/move.h"
+#include "chessgame/pieces.h"
+
+namespace ChessGame
+{
+    namespace bp = boost::process;
+
+    class UCI
+    {
+    public:
+        UCI() = default;
+        explicit UCI(std::string_view enginePath);
+        void loadEngine(std::string_view enginePath);
+        ~UCI();
+
+        void newGame(const Position &pieces = Position());
+        void applyMove(const Move &move);
+        eval_t evaluation();
+        Move bestMove();
+        void stop();
+        void start();
+
+    private:
+        bp::ipstream m_engInput;
+        bp::opstream m_engOutput;
+        bp::child m_engine;
+        bool m_running;
+    };
+} // namespace ChessGame
