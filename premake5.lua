2df135066ae9c56f45a168876f149434000b7dec
Michael Jacobs <M.S.Jacobs2727@gmail.com>
2024-01-01 20:04:12 -0500

    change build system

---
+++ premake5.lua
@@ -0,0 +1,33 @@
+workspace "JChess"
+    configurations {"Debug", "Release"}
+
+project "JChess"
+    kind "StaticLib"
+    language "C++"
+    targetdir "build/bin/%{cfg.buildcfg}"
+    objdir "build/obj"
+
+    links {"pq", "pqxx"}
+
+    files {"src/**.h", "src/**.cpp"}
+    removefiles {"src/engine/*"}
+
+    pqxxdir = os.findheader("pqxx/pqxx")
+    includedirs({"src", pqxxdir})
+
+    filter "configurations:Debug"
+        defines { "DEBUG" }
+        symbols "On"
+
+    filter "configurations:Release"
+        defines { "NDEBUG" }
+        optimize "On"
+    
+    filter "action:gmake2"
+        buildoptions {"-std=c++20", "-Wall", "-Waddress"}
+    
+    filter {"action:gmake2", "configurations:Debug"}
+        buildoptions {"-g", "-Og"}
+    
+    filter {"action:gmake2", "configurations:Release"}
+        buildoptions {"-O3"}
