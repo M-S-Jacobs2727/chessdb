9c21ba1a10ec52e1fe6e098b32618434e3a7f7ab
Michael Jacobs <M.S.Jacobs2727@gmail.com>
2024-01-15 13:20:03 -0500

    add offset operations

--- src/internal/logic/offset.h
+++ src/internal/logic/offset.h
@@ -1,3 +1,5 @@
+#include "internal/game/square.h"
+#include <optional>
 
 namespace ChessGame
 {
@@ -5,5 +7,46 @@ namespace ChessGame
     {
         int file = 0;
         int rank = 0;
+
+        constexpr Offset operator+(const Offset &other) const
+        {
+            return {
+                .file = file + other.file,
+                .rank = rank + other.rank,
+            };
+        }
+
+        constexpr Offset operator*(int factor) const
+        {
+            return {
+                .file = file * factor,
+                .rank = rank * factor,
+            };
+        }
+
+        constexpr Offset operator/(int factor) const
+        {
+            return {
+                .file = file / factor,
+                .rank = rank / factor,
+            };
+        }
+
+        constexpr Offset operator-() const
+        {
+            return (*this) * -1;
+        }
+
+        constexpr Offset operator-(const Offset &other) const
+        {
+            return (*this) + (-other);
+        }
+
+        constexpr std::optional<Square> operator()(const Square &square) const
+        {
+            Square s{square.file + file,
+                     square.rank + rank};
+            return s.valid() ? std::make_optional(s) : std::nullopt;
+        }
     };
 }
