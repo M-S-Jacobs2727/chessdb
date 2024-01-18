2fe6d2682275f382e7f6b458979b0a7f8e65a362
Michael Jacobs <M.S.Jacobs2727@gmail.com>
2024-01-18 09:27:53 -0500

    implement State class

--- src/internal/logic/state.h
+++ src/internal/logic/state.h
@@ -1,10 +1,9 @@
 #pragma once
 
-#include <array>
 #include <cstdint>
 #include <optional>
 
-#include "internal/logic/castleRights.h"
+#include "internal/logic/castling.h"
 #include "internal/logic/move.h"
 #include "internal/logic/piece.h"
 #include "internal/logic/position.h"
@@ -14,22 +13,27 @@ namespace ChessGame
     class State
     {
     public:
+        Position position;
+        uint8_t fullTurnCounter;
+        uint8_t halfTurnCounter;
+        Color turn;
+        CastleRights castleRights;
+        std::optional<Square> enPassant;
+
+    public:
         explicit State(std::string_view fenString = INITFEN);
         void applyMove(const Move &move);
         Move applyPGNMove(std::string_view pgnMove);
         Move applyUCIMove(std::string_view uciMove);
-
-    public:
-        Position position;
-        uint8_t fullTurnCounter;
-        uint8_t halfTurnCounter;
-        Color turn = Color::White;
-        CastleRights castleRights;
-        std::optional<Square> enPassant;
-
-    private:
-        void determineFromSquare(Move &move);
-        void determineExtra(const Move &move);
-        uint8_t homeRank(Color color);
     };
+
+    Square computeFromSquare(Square to,
+                             Piece piece,
+                             const Position &pos,
+                             std::string_view partialFrom);
+    Move interpretPGNMove(std::string_view pgnMove,
+                          const Position &pos,
+                          Color turn);
+    Move interpretUCIMove(std::string_view uciMove, const State &state);
+
 } // namespace ChessGame
