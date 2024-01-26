691482acee8499c7727046774d4f7b99742e8239
Michael Jacobs <M.S.Jacobs2727@gmail.com>
2024-01-26 13:43:02 -0500

    refactor premake5 build system

--- premake5.lua
+++ premake5.lua
@@ -1,20 +1,14 @@
 workspace "JChess"
-    configurations {"Debug", "Release"}
-
-project "JChess"
-    kind "StaticLib"
+    architecture "x64"
     language "C++"
-    targetdir "build/bin/%{cfg.buildcfg}"
-    objdir "build/obj"
 
-    links {"pq", "pqxx"}
+    configurations {"Debug", "Release"}
+    
+    includedirs "src"
+    location "build"
 
-    files {"src/**.h", "src/**.cpp"}
-    removefiles {"src/engine/*", "src/internal/game/*", "src/fileFormat/*", "src/database/*"}
-
-    pqxxdir = os.findheader("pqxx/pqxx")
-    includedirs({"src", pqxxdir})
-
+    locdir = "build/%{prj.name}/" --%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+    
     filter "configurations:Debug"
         defines { "DEBUG" }
         symbols "On"
@@ -31,3 +25,64 @@ project "JChess"
     
     filter {"action:gmake2", "configurations:Release"}
         buildoptions {"-O3"}
+
+    filter {}
+    
+project "jchess-core"
+    kind "StaticLib"
+
+    location(locdir)
+    targetdir "%{prj.location}"
+    objdir "%{prj.location}/obj"
+
+    files {"src/internal/**.cpp", "src/internal/**.h"}
+    
+project "jchess-engine"
+    kind "StaticLib"
+
+    location(locdir)
+    targetdir "%{prj.location}"
+    objdir "%{prj.location}/obj"
+
+    links {"jchess-core"}
+
+    files {"src/engine/**.cpp", "src/engine/**.h"}
+    
+project "jchess-database"
+    kind "StaticLib"
+
+    location(locdir)
+    targetdir "%{prj.location}"
+    objdir "%{prj.location}/obj"
+
+    pqxxdir = os.findheader("pqxx/pqxx")
+    includedirs({"src", pqxxdir})
+
+    links {"jchess-core", "pq", "pqxx"}
+
+    files {"src/database/**.cpp", "src/database/**.h"}
+
+-- project "jchess-lib"
+--     kind "StaticLib"
+
+--     location(locdir)
+--     targetdir "%{prj.location}"
+--     objdir "%{prj.location}/obj"
+
+--     links {"jchess-core", "jchess-engine", "jchess-database"}
+
+--     files {"src/interface/**.cpp", "src/interface/**.h"}
+
+project "jchess-test"
+    kind "ConsoleApp"
+
+    location(locdir)
+    targetdir "%{prj.location}"
+    objdir "%{prj.location}/obj"
+
+    gtestdir = os.findheader("gtest/gtest.h")
+    includedirs({"src", gtestdir})
+
+    links {"jchess-core", "jchess-engine", "jchess-database", "gtest_main"}
+
+    files "test/**.cpp"
