113072a77fbca424980dd9e06e7a22131b4be3eb
Michael Jacobs <M.S.Jacobs2727@gmail.com>
2024-01-28 14:01:45 -0500

    add basic assertion tests to Castling

--- test/internal/testCastling.cpp
+++ test/internal/testCastling.cpp
@@ -3,3 +3,13 @@
 
 namespace cstl = ChessGame::Castling;
 
+TEST(CastlingTest, BasicAssertions)
+{
+    using ChessGame::Color, cstl::Side;
+
+    cstl::Rights rights;
+    ASSERT_TRUE(rights.get(Color::White, Side::QUEEN));
+    ASSERT_TRUE(rights.get(Color::White, Side::KING));
+    ASSERT_TRUE(rights.get(Color::Black, Side::QUEEN));
+    ASSERT_TRUE(rights.get(Color::Black, Side::KING));
+}
