a63c032834f0c0559e078d5d88e4e8c9623397c1
Michael Jacobs <M.S.Jacobs2727@gmail.com>
2024-01-28 14:02:03 -0500

    add to/from square tests for Castling

--- test/internal/testCastling.cpp
+++ test/internal/testCastling.cpp
@@ -13,3 +13,48 @@ TEST(CastlingTest, BasicAssertions)
     ASSERT_TRUE(rights.get(Color::Black, Side::QUEEN));
     ASSERT_TRUE(rights.get(Color::Black, Side::KING));
 }
+
+TEST(CastlingTest, Squares)
+{
+    using ChessGame::Square, ChessGame::Color, ChessGame::Castling::Side, ChessGame::Castling::Castle;
+    Square sq, sq2;
+
+    sq = cstl::kingToSquare(Castle{Color::White, Side::QUEEN});
+    sq2 = Square("c1");
+    ASSERT_EQ(sq, sq2);
+    sq = cstl::kingToSquare(Castle{Color::White, Side::KING});
+    sq2 = Square("g1");
+    ASSERT_EQ(sq, sq2);
+    sq = cstl::kingToSquare(Castle{Color::Black, Side::QUEEN});
+    sq2 = Square("c8");
+    ASSERT_EQ(sq, sq2);
+    sq = cstl::kingToSquare(Castle{Color::Black, Side::KING});
+    sq2 = Square("g8");
+    ASSERT_EQ(sq, sq2);
+
+    sq = cstl::rookToSquare(Castle{Color::White, Side::QUEEN});
+    sq2 = Square("d1");
+    ASSERT_EQ(sq, sq2);
+    sq = cstl::rookToSquare(Castle{Color::White, Side::KING});
+    sq2 = Square("f1");
+    ASSERT_EQ(sq, sq2);
+    sq = cstl::rookToSquare(Castle{Color::Black, Side::QUEEN});
+    sq2 = Square("d8");
+    ASSERT_EQ(sq, sq2);
+    sq = cstl::rookToSquare(Castle{Color::Black, Side::KING});
+    sq2 = Square("f8");
+    ASSERT_EQ(sq, sq2);
+
+    sq = cstl::rookFromSquare(Castle{Color::White, Side::QUEEN});
+    sq2 = Square("a1");
+    ASSERT_EQ(sq, sq2);
+    sq = cstl::rookFromSquare(Castle{Color::White, Side::KING});
+    sq2 = Square("h1");
+    ASSERT_EQ(sq, sq2);
+    sq = cstl::rookFromSquare(Castle{Color::Black, Side::QUEEN});
+    sq2 = Square("a8");
+    ASSERT_EQ(sq, sq2);
+    sq = cstl::rookFromSquare(Castle{Color::Black, Side::KING});
+    sq2 = Square("h8");
+    ASSERT_EQ(sq, sq2);
+}
