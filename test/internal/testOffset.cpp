13b149cc025c265b757b92c502f2e5019fe2cb56
Michael Jacobs <M.S.Jacobs2727@gmail.com>
2024-01-28 14:00:06 -0500

    add basic tests for Offset

--- test/internal/testOffset.cpp
+++ test/internal/testOffset.cpp
@@ -3,3 +3,13 @@
 
 using ChessGame::Offset;
 
+TEST(OffsetTest, BasicAssertions)
+{
+    Offset offset;
+    ASSERT_EQ(offset.file, 0);
+    ASSERT_EQ(offset.rank, 0);
+
+    Offset offset2{-1, 8};
+    ASSERT_EQ(offset2.file, -1);
+    ASSERT_EQ(offset2.rank, 8);
+}
