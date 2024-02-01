c57330d0bc3e97c76ade1ffa7b47b9058b17482a
Michael Jacobs <M.S.Jacobs2727@gmail.com>
2024-01-31 20:51:05 -0500

    change square fields to signed ints

--- src/internal/logic/square.h
+++ src/internal/logic/square.h
@@ -7,11 +7,11 @@ namespace ChessGame
 {
     struct Square
     {
-        uint32_t file = 0; // 0->a, 7->h
-        uint32_t rank = 0; // 0->1, 7->8
+        int file = 0; // 0->a, 7->h
+        int rank = 0; // 0->1, 7->8
 
         constexpr Square() = default;
-        constexpr Square(uint32_t file, uint32_t rank)
+        constexpr Square(int file, int rank)
             : file(file), rank(rank) {}
 
         constexpr inline bool operator==(const Square &other) const
@@ -27,8 +27,8 @@ struct std::hash<ChessGame::Square>
 {
     inline size_t operator()(const ChessGame::Square &square) const noexcept
     {
-        size_t h1 = std::hash<size_t>{}(static_cast<size_t>(square.file));
-        size_t h2 = std::hash<size_t>{}(static_cast<size_t>(square.rank));
-        return (h1 << 1) ^ h2;
+        size_t h1 = std::hash<int>{}(square.file);
+        size_t h2 = std::hash<int>{}(square.rank);
+        return (h1 << 4) ^ h2;
     }
 };
