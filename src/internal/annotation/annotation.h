dc6e522deea1383f59b0271769a505880d1cb6b1
Michael Jacobs <M.S.Jacobs2727@gmail.com>
2024-02-02 16:06:01 -0500

    update Annotations

--- src/internal/annotation/annotation.h
+++ src/internal/annotation/annotation.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #include <optional>
+#include <string>
 #include <string_view>
+#include <unordered_map>
 
 namespace ChessGame
 {
@@ -14,10 +16,37 @@ namespace ChessGame
         Blunder,
         Interesting,
         Dubious,
-        None2,
     };
 
-    Annotation readAnnotation(const std::string_view &str);
-    std::optional<std::string_view> toPGN(const Annotation annotation);
-    std::optional<std::string_view> toStr(const std::optional<Annotation> annotation);
+    namespace Annotations
+    {
+        using namespace std::string_view_literals;
+        const inline std::unordered_map<std::string_view, Annotation> fromPGN = {
+            {""sv, Annotation::None},
+            {"!"sv, Annotation::Good},
+            {"?"sv, Annotation::Mistake},
+            {"!!"sv, Annotation::Brilliant},
+            {"??"sv, Annotation::Blunder},
+            {"!?"sv, Annotation::Interesting},
+            {"?!"sv, Annotation::Dubious}};
+
+        using namespace std::string_literals;
+        const inline std::unordered_map<Annotation, std::string> toPGN = {
+            {Annotation::None, ""s},
+            {Annotation::Good, "!"s},
+            {Annotation::Mistake, "?"s},
+            {Annotation::Brilliant, "!!"s},
+            {Annotation::Blunder, "??"s},
+            {Annotation::Interesting, "!?"s},
+            {Annotation::Dubious, "?!"s}};
+
+        const inline std::unordered_map<Annotation, std::string> toWord = {
+            {Annotation::None, ""s},
+            {Annotation::Good, "good"s},
+            {Annotation::Mistake, "mistake"s},
+            {Annotation::Brilliant, "brilliant"s},
+            {Annotation::Blunder, "blunder"s},
+            {Annotation::Interesting, "interesting"s},
+            {Annotation::Dubious, "dubious"s}};
+    } // namespace Annotations
 } // namespace ChessGame
