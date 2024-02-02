53c710f49b72766c1288a37f90bc8d73ce792968
Michael Jacobs <M.S.Jacobs2727@gmail.com>
2024-02-02 16:32:17 -0500

    update and simplify time files

--- src/internal/time/datetime.h
+++ src/internal/time/datetime.h
@@ -6,16 +6,5 @@
 
 namespace ChessGame
 {
-    struct Datetime
-    {
-        uint16_t year = 0u;
-        uint8_t month = 0u;
-        uint8_t day = 0u;
-        uint8_t hour = 0u;
-        uint8_t minute = 0u;
-        uint8_t second = 0u;
-
-        Datetime() = default;
-        Datetime(const std::string &dateString, const std::string &timeString);
-    };
+    using std::chrono::utc_time;
 } // namespace ChessGame
