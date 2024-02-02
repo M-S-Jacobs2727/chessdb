4ea0599c5939cc9884a34e82c1788710105311d5
Michael Jacobs <M.S.Jacobs2727@gmail.com>
2024-02-02 15:42:30 -0500

    update State

--- src/internal/logic/state.h
+++ src/internal/logic/state.h
@@ -8,7 +8,6 @@
 #include "internal/logic/castling.h"
 #include "internal/logic/move.h"
 #include "internal/logic/piece.h"
-#include "internal/logic/position.h"
 
 namespace ChessGame
 {
@@ -25,28 +24,12 @@ namespace ChessGame
         Color turn;
         Castling::Rights castleRights;
         std::optional<Square> enPassant;
-
-    private:
-        std::shared_ptr<Board> m_pos;
-
-    public:
         Attacks attacks;
 
     public:
-        explicit State(std::string_view fenString = initFENFull);
-
+        State();
         /// @brief Applies a move to the board state
         /// @param move
         void applyMove(const Move &move);
-
-        /// @brief Applies a PGN-formatted move to the board state
-        /// @param pgnMove The PGN-formatted move
-        /// @return A full description of the move applied
-        Move applyPGNMove(std::string_view pgnMove);
-
-        /// @brief Applies a move formatted in long algebraic notation to the board state
-        /// @param pgnMove The move formatted in long algebraic notation
-        /// @return A full description of the move applied
-        Move applyUCIMove(std::string_view uciMove);
     };
 } // namespace ChessGame
