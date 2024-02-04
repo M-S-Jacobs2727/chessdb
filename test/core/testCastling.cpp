d5ea0f1220816d655681dc177601c7c81aeaa5cd
Michael Jacobs <M.S.Jacobs2727@gmail.com>
2024-02-04 16:49:26 -0500

    update tests for Castling

--- test/core/testCastling.cpp
+++ test/core/testCastling.cpp
@@ -14,51 +14,6 @@ TEST(CastlingTest, BasicAssertions)
     EXPECT_TRUE(rights.get(Color::Black, Side::KING));
 }
 
-TEST(CastlingTest, Squares)
-{
-    using JChess::Square, JChess::Color, JChess::Castling::Side;
-    Square sq, sq2;
-
-    sq = cstl::kingToSquare(Color::White, Side::QUEEN);
-    sq2 = Square("c1");
-    EXPECT_EQ(sq, sq2);
-    sq = cstl::kingToSquare(Color::White, Side::KING);
-    sq2 = Square("g1");
-    EXPECT_EQ(sq, sq2);
-    sq = cstl::kingToSquare(Color::Black, Side::QUEEN);
-    sq2 = Square("c8");
-    EXPECT_EQ(sq, sq2);
-    sq = cstl::kingToSquare(Color::Black, Side::KING);
-    sq2 = Square("g8");
-    EXPECT_EQ(sq, sq2);
-
-    sq = cstl::rookToSquare(Color::White, Side::QUEEN);
-    sq2 = Square("d1");
-    EXPECT_EQ(sq, sq2);
-    sq = cstl::rookToSquare(Color::White, Side::KING);
-    sq2 = Square("f1");
-    EXPECT_EQ(sq, sq2);
-    sq = cstl::rookToSquare(Color::Black, Side::QUEEN);
-    sq2 = Square("d8");
-    EXPECT_EQ(sq, sq2);
-    sq = cstl::rookToSquare(Color::Black, Side::KING);
-    sq2 = Square("f8");
-    EXPECT_EQ(sq, sq2);
-
-    sq = cstl::rookFromSquare(Color::White, Side::QUEEN);
-    sq2 = Square("a1");
-    EXPECT_EQ(sq, sq2);
-    sq = cstl::rookFromSquare(Color::White, Side::KING);
-    sq2 = Square("h1");
-    EXPECT_EQ(sq, sq2);
-    sq = cstl::rookFromSquare(Color::Black, Side::QUEEN);
-    sq2 = Square("a8");
-    EXPECT_EQ(sq, sq2);
-    sq = cstl::rookFromSquare(Color::Black, Side::KING);
-    sq2 = Square("h8");
-    EXPECT_EQ(sq, sq2);
-}
-
 TEST(CastlingTest, FENConstructor)
 {
     using JChess::Color, JChess::Castling::Side;
